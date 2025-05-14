967fc127eab4967e95a0294c4d6e8b11bade5080
Bolgeg <ricardobolgeg@gmail.com>
2025-05-14 12:47:17 +0200

    Solve second layer

--- classes/cube_solver.gd
+++ classes/cube_solver.gd
@@ -17,6 +17,82 @@ func add_step(cube_state:CubeState,step:Dictionary)->CubeState:
 	next_steps.append(step)
 	return cube_state.rotated(step.axis,step.clockwise)
 
+func move_bottom_to_side(cube_state:CubeState,face:Vector3)->CubeState:
+	var side_face=face.rotated(Vector3.MODEL_TOP,-PI/2).round()
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":side_face,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":side_face,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":face,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":face,
+		"clockwise":true,
+	})
+	return cube_state
+
+func move_bottom_to_side_flipped(cube_state:CubeState,face:Vector3)->CubeState:
+	var side_face=face.rotated(Vector3.MODEL_TOP,-PI/2).round()
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":face,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":face,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":side_face,
+		"clockwise":true,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":Vector3.MODEL_BOTTOM,
+		"clockwise":false,
+	})
+	cube_state=add_step(cube_state,{
+		"axis":side_face,
+		"clockwise":false,
+	})
+	return cube_state
+
 func fill_next_steps(cube_state:CubeState):
 	var solved_cube:=CubeState.new(cube_state.size)
 	
@@ -50,12 +126,10 @@ func fill_next_steps(cube_state:CubeState):
 					})
 			
 			var target_face=get_position_side_face(target_position)
-			
 			while true:
 				origin_position=cube_state.find_position_of(solved_cube.get_colors_at(target_position))
 				if get_position_side_face(origin_position)==target_face:
 					break
-				
 				cube_state=add_step(cube_state,{
 						"axis":Vector3.MODEL_BOTTOM,
 						"clockwise":false,
@@ -88,6 +162,118 @@ func fill_next_steps(cube_state:CubeState):
 						"clockwise":true,
 					})
 			return
+	
+	for i in range(4):
+		var target_position=cube_state.get_position_rotated(
+			Vector3.MODEL_TOP+Vector3.MODEL_FRONT+Vector3.MODEL_RIGHT,i
+			)
+		if not compare_cube_position_equal(cube_state,solved_cube,target_position):
+			var origin_position=cube_state.find_position_of(solved_cube.get_colors_at(target_position))
+			if origin_position.y==1:
+				var origin_face=get_position_side_face(origin_position)
+				cube_state=add_step(cube_state,{
+					"axis":origin_face,
+					"clockwise":false,
+				})
+				cube_state=add_step(cube_state,{
+					"axis":Vector3.MODEL_BOTTOM,
+					"clockwise":false,
+				})
+				cube_state=add_step(cube_state,{
+					"axis":origin_face,
+					"clockwise":true,
+				})
+			
+			var target_face=get_position_side_face(target_position)
+			while true:
+				origin_position=cube_state.find_position_of(solved_cube.get_colors_at(target_position))
+				if get_position_side_face(origin_position)==target_face:
+					break
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":false,
+					})
+			
+			if cube_state.get_colors_at(origin_position)[Vector3.MODEL_BOTTOM]==Vector3.MODEL_TOP:
+				cube_state=add_step(cube_state,{
+						"axis":target_face,
+						"clockwise":false,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":false,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":false,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":target_face,
+						"clockwise":true,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":true,
+					})
+			
+			if cube_state.get_colors_at(origin_position)[target_face]==Vector3.MODEL_TOP:
+				cube_state=add_step(cube_state,{
+						"axis":target_face,
+						"clockwise":false,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":false,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":target_face,
+						"clockwise":true,
+					})
+			else:
+				var side_face=target_face.rotated(Vector3.MODEL_TOP,-PI/2).round()
+				cube_state=add_step(cube_state,{
+						"axis":side_face,
+						"clockwise":true,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":true,
+					})
+				cube_state=add_step(cube_state,{
+						"axis":side_face,
+						"clockwise":false,
+					})
+			
+			return
+	
+	for i in range(4):
+		var target_position=cube_state.get_position_rotated(
+			Vector3.MODEL_FRONT+Vector3.MODEL_RIGHT,i
+			)
+		if not compare_cube_position_equal(cube_state,solved_cube,target_position):
+			var origin_position=cube_state.find_position_of(solved_cube.get_colors_at(target_position))
+			if origin_position.y==0:
+				var origin_face=get_position_side_face(origin_position)
+				cube_state=move_bottom_to_side(cube_state,origin_face)
+			
+			var target_face=get_position_side_face(target_position)
+			while true:
+				origin_position=cube_state.find_position_of(solved_cube.get_colors_at(target_position))
+				if get_position_side_face(origin_position)==target_face:
+					break
+				cube_state=add_step(cube_state,{
+						"axis":Vector3.MODEL_BOTTOM,
+						"clockwise":false,
+					})
+			
+			if cube_state.get_colors_at(origin_position)[target_face]==target_face:
+				cube_state=move_bottom_to_side(cube_state,target_face)
+			else:
+				cube_state=move_bottom_to_side_flipped(cube_state,target_face)
+			
+			return
+	
+	
 
 func next_step(cube_state:CubeState)->Dictionary:
 	var output:={
